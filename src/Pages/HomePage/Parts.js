687c4f9e36bb0f92e5eaae814fbe2233e4b1ab27
unknown <you@example.com>
2022-05-24 11:22:35 +0600

    add tools and parts

---
+++ src/Pages/HomePage/Parts.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Parts = ({tool}) => {
+    const { name, img, price, } = tool;
+    return (
+        <div className=''>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <figure class="px-10 pt-10">
+                    <img src={img} alt="Shoes" class="rounded-xl" />
+                </figure>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Parts;
---
+++ src/Pages/HomePage/Tools.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Parts from './Parts';
+
+const Tools = () => {
+    const [tools, setTools] = useState([]);
+    fetch('product.json')
+        .then(res => res.json())
+        .then(data => setTools(data))
+    
+    return (
+        <div>
+            <div className='text-center my-10'>
+                <h2 className='text-primary text-4xl font-bold uppercase'>Tools & Parts</h2>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    tools.map(tool => <Parts
+                    key={tool.id} tool={tool}
+                    ></Parts>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Tools;
